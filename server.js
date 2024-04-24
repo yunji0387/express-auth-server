0bcc58508ca0086a9d4e2ff8cf54215cae6697ba
yunji0387 <yunji0387@gmail.com>
2024-04-24 13:10:09 -0500

    Update server.js to include MongoDB connection and route handling

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import { PORT, URI } from "./config/index.js";
+// import App from "./routes/index.js";
+
+const server = express();
+
+server.use(cors());
+server.disable("x-powered-by");
+server.use(cookieParser());
+server.use(express.urlencoded({ extended: false }));
+server.use(express.json());
+
+mongoose.promise = global.Promise;
+mongoose.set("strictQuery", false);
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
+
+server.use(App);
+
+server.listen(PORT, () =>
+    console.log(`Server is running on port ${PORT}`),
+);
