928bd989e029ff09e5d32568e6c97af2764395d9
yunji0387 <yunji0387@gmail.com>
2024-04-24 17:50:45 -0500

    Add Blacklist model for token revocation

---
+++ models/Blacklist.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const BlacklistSchema = new mongoose.Schema({
+    token: {
+        type: String,
+        required: true,
+        ref: "User",
+    },
+}, { timestamps: true });
+
+export default mongoose.model("Blacklist", BlacklistSchema);
