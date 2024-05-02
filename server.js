b8d083356600ff157f51c0ede57ee9798c0c3437
yunji0387 <yunji0387@gmail.com>
2024-05-01 22:40:11 -0500

    Refactor CORS configuration in server.js to allow multiple origins

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ import App from "./routes/index.js";
 
 const server = express();
 
-server.use(cors());
+// Define CORS options
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://next-form-app-pi.vercel.app'], // List of allowed origins
+    credentials: true, // Allow credentials
+    optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+server.use(cors(corsOptions)); // Use CORS with specified options
 server.disable("x-powered-by");
 server.use(cookieParser());
 server.use(express.urlencoded({ extended: false }));
@@ -16,7 +23,10 @@ server.use(express.json());
 mongoose.promise = global.Promise;
 mongoose.set("strictQuery", false);
 mongoose.connect(URI).then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log(err));
+    .catch((err) => {
+        console.error("Failed to connect to MongoDB", err);
+        process.exit(1); // Exit process with an error code
+    });
 
 server.use(App);
 
