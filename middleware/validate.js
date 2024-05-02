690bf3db68fa3c0e890851097426296e65793c42
yunji0387 <yunji0387@gmail.com>
2024-05-02 12:54:57 -0500

    Refactor error handling in authentication middleware to return first error message

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,11 +1,27 @@
-import  { validationResult } from 'express-validator';
+import { validationResult } from 'express-validator';
+
+// const Validate = (req, res, next) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         let error = {};
+//         errors.array().map((err) => (error[err.param] = err.msg));
+//         return res.status(422).json({ error });
+//     }
+//     next();
+// };
 
 const Validate = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        let error = {};
-        errors.array().map((err) => (error[err.param] = err.msg));
-        return res.status(422).json({ error });
+        // Retrieve the first error message
+        const firstErrorMessage = errors.array()[0].msg;
+
+        // Return it under a 'message' key
+        return res.status(422).json({
+            error: {
+                message: firstErrorMessage
+            }
+        });
     }
     next();
 };
