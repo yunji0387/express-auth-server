eccc9d0b1f227e94479f3e6bc2892ebe57e843a0
yunji0387 <yunji0387@gmail.com>
2024-05-02 12:55:13 -0500

    Refactor validation middleware to remove commented out code

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,15 +1,5 @@
 import { validationResult } from 'express-validator';
 
-// const Validate = (req, res, next) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//         let error = {};
-//         errors.array().map((err) => (error[err.param] = err.msg));
-//         return res.status(422).json({ error });
-//     }
-//     next();
-// };
-
 const Validate = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
