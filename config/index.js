5ac71e2d9b77a1ecbb4db86ebe4cebb06a468451
yunji0387 <yunji0387@gmail.com>
2024-06-09 11:22:41 -0500

    chore: Update nodemailer configuration and dependencies

--- config/index.js
+++ config/index.js
@@ -1,6 +1,18 @@
 import * as dotenv from "dotenv";
+import nodemailer from 'nodemailer';
+
 dotenv.config();
 
 const { URI, PORT, SECRET_ACCESS_TOKEN } = process.env;
 
 export { URI, PORT, SECRET_ACCESS_TOKEN };
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+export default transporter;
