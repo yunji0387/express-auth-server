54b584ed319e7dbe4037582561bde28d49c30140
yunji0387 <yunji0387@gmail.com>
2024-06-09 19:33:01 -0500

    chore: Remove commented out code and update password reset form view

--- controllers/auth.js
+++ controllers/auth.js
@@ -277,7 +277,6 @@ export async function ResetPassword(req, res) {
             });
         }
 
-        // user.password = await bcrypt.hash(password, 10);
         user.password = password;
         user.resetPasswordToken = undefined;
         user.resetPasswordExpires = undefined;
