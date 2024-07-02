d3b4ce2a817369b5d2a75c3d465203a4488589e7
yunji0387 <yunji0387@gmail.com>
2024-07-01 19:31:20 -0500

    chore: Remove unused code and update nodemailer configuration

--- config/index.js
+++ config/index.js
@@ -1,12 +1,9 @@
 import * as dotenv from 'dotenv';
 import nodemailer from 'nodemailer';
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import User from '../models/User.js';
 
 dotenv.config();
 
-const { URI, PORT, SECRET_ACCESS_TOKEN, EMAIL_USER, NODEMAILER_PASS, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, COOKIE_KEY } = process.env;
+const { URI, PORT, SECRET_ACCESS_TOKEN, EMAIL_USER, NODEMAILER_PASS } = process.env;
 
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -16,42 +13,4 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-// Passport Configuration
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-      let user = await User.findOne({ googleId: profile.id });
-      if (!user) {
-        user = new User({
-          googleId: profile.id,
-          first_name: profile.name.givenName,
-          last_name: profile.name.familyName,
-          email: profile.emails[0].value
-        });
-        await user.save();
-      }
-      done(null, user);
-    } catch (err) {
-      done(err, null);
-    }
-  }
-));
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err, null);
-  }
-});
-
-export { URI, PORT, SECRET_ACCESS_TOKEN, transporter, COOKIE_KEY, passport };
+export { URI, PORT, SECRET_ACCESS_TOKEN, transporter };
