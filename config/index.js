2c61ef6f25b423c0c327eaebc824ae54cc6f46e1
yunji0387 <yunji0387@gmail.com>
2024-07-01 19:13:19 -0500

    feat: Add Google OAuth for authentication

--- config/index.js
+++ config/index.js
@@ -1,9 +1,12 @@
 import * as dotenv from 'dotenv';
 import nodemailer from 'nodemailer';
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import User from '../models/User.js';
 
 dotenv.config();
 
-const { URI, PORT, SECRET_ACCESS_TOKEN, EMAIL_USER, NODEMAILER_PASS } = process.env;
+const { URI, PORT, SECRET_ACCESS_TOKEN, EMAIL_USER, NODEMAILER_PASS, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, COOKIE_KEY } = process.env;
 
 const transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -13,4 +16,42 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-export { URI, PORT, SECRET_ACCESS_TOKEN, transporter };
+// Passport Configuration
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ googleId: profile.id });
+      if (!user) {
+        user = new User({
+          googleId: profile.id,
+          first_name: profile.name.givenName,
+          last_name: profile.name.familyName,
+          email: profile.emails[0].value
+        });
+        await user.save();
+      }
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+export { URI, PORT, SECRET_ACCESS_TOKEN, transporter, COOKIE_KEY, passport };
